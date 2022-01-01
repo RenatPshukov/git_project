c2422a3b0359f12056ca84aaa2ecbc0202bff181
Renat <renat_pshukov06@mail.ru>
2022-01-01 15:15:23 +0300

    update

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QGridLayout
 from PyQt5.QtGui import QPainter, QPixmap, QPen, QColor
 from PyQt5.QtCore import Qt
@@ -27,8 +29,7 @@ class Test(QMainWindow):
         painter = QPainter(self.label.pixmap())
         pen = QPen()
         pen.setWidth(3)
-        # pen.setColor(QColor(*[randint(0, 255) for i in range(3)]))
-        pen.setColor(QColor('#ffff00'))
+        pen.setColor(QColor(*[randint(0, 255) for i in range(3)]))
         painter.setPen(pen)
         painter.drawEllipse(x, y, w, h)
         painter.end()
@@ -36,7 +37,6 @@ class Test(QMainWindow):
 
 
 if __name__ == '__main__':
-    import sys
     app = QApplication(sys.argv)
     w = Test()
     w.show()
